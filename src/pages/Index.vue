714e0ecb105573addb3c259c7f53a833ef593f53
Rukaroa <rukaroa@gmail.com>
2020-05-25 19:32:20 -0500

    base grids added

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,19 +1,44 @@
 <template>
   <div class="q-gutter-sm absolute-center">
-    <q-card class="main-card">
-      <q-card-section>
-        <div class="text-h5">
-          <q-icon name="fas fa-address-card"/> Resume and Portfolio
-        </div>
-      </q-card-section>
-      <q-card-actions class="absolute-bottom">
-        <q-btn
-          v-for="(label, index) in buttonLabels"
-          :key="index"
-          :label="label"
-          flat
-        />
-      </q-card-actions>
+    <q-card class="main-card row">
+      <div class="col-7">
+        <q-card-section class="col-12">
+          <div class="text-h5">
+            <q-icon name="fas fa-address-card"/> Resume and Portfolio
+          </div>
+        </q-card-section>
+        <q-card-section>
+          <div>
+            Name
+          </div>
+        </q-card-section>
+        <q-card-section>
+          <div>
+            Contact
+          </div>
+        </q-card-section>
+        <q-card-section>
+          <div>
+            Intro
+          </div>
+        </q-card-section>
+        <q-card-section>
+          <div>
+            Instructions
+          </div>
+        </q-card-section>
+        <q-card-actions class="absolute-bottom">
+          <q-btn
+            v-for="(label, index) in buttonLabels"
+            :key="index"
+            :label="label"
+            flat
+          />
+        </q-card-actions>
+      </div>
+      <div class="col-5">
+        Information goes here
+      </div>
     </q-card>
   </div>
 </template>
