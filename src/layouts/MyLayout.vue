b005eebb38deadb63e18e42eb7572b24bae22bbd
Rukaroa <rukaroa@gmail.com>
2020-05-25 19:13:21 -0500

    removed header/drawer; background color change

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -1,68 +1,12 @@
 <template>
-  <q-layout view="hHh Lpr lff">
-    <q-header elevated>
+  <q-layout view="hHh Lpr lff" class="bg-dark">
+    <!-- <q-header elevated class="">
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          @click="leftDrawerOpen = !leftDrawerOpen"
-          icon="fas fa-bars"
-          aria-label="Menu"
-        />
-
         <q-toolbar-title class="absolute-center">
           Portfolio and Resume
         </q-toolbar-title>
-
       </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      mini-to-overlay
-      :mini="miniState"
-      @mouseover="miniState = false"
-      @mouseleave="miniState = true"
-      content-class="bg-grey-2"
-    >
-      <q-list>
-        <q-item clickable>
-          <q-item-section avatar>
-            <q-icon name="fas fa-address-card" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>About</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable>
-          <q-item-section avatar>
-            <q-icon name="fas fa-file-contract" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Experience</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable>
-          <q-item-section avatar>
-            <q-icon name="fas fa-tools" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Projects</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable>
-          <q-item-section avatar>
-            <q-icon name="fas fa-envelope" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Contact</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-drawer>
+    </q-header> -->
 
     <q-page-container>
       <router-view />
@@ -76,8 +20,6 @@ export default {
 
   data () {
     return {
-      miniState: true,
-      leftDrawerOpen: false
     }
   }
 }
